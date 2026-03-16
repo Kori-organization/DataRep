3e5e488075539dbb902b8cbb9caa2aef3250c53c
Davi Arakaki Oliveira De Souza <davisouza-ieg@instituto.ad>
2026-03-16 11:44:39 -0300

    Adding the import of cors

--- kori-api/app/main.py
+++ kori-api/app/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routes.student import router as student_router
 from app.routes.professor import router as professor_router
 from app.routes.chat import router as chat_router
 
 app = FastAPI()
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -16,7 +18,6 @@ app.add_middleware(
 @app.get("/")
 def root():
     return {"message": "API funcionando 🚀"}
-
 
 app.include_router(student_router)
 app.include_router(professor_router)
